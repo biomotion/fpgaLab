9cb43890b6404f6ffd3c512a9d76f5330f1f8d86
biomotion <35159458+biomotion@users.noreply.github.com>
2019-03-25 20:21:41 +0800

    create lab4_1

---
+++ lab4_1/lab4_1.v
@@ -0,0 +1,13 @@
+module lab4_1(Q, R, S, Clk);
+	input Clk, R, S;
+	output Q;
+	
+	wire R_g, S_g, Qa, Qb;
+	
+	assign R_g = R&Clk;
+	assign S_g = S&Clk;
+	assign Qa = ~{R_g|Qb};
+	assign Qb = ~{S_g|Qa};
+	
+	assign Q = Qa;
+endmodule 
